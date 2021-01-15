203f1d7b1476d748bed4d7a6ef2ca9b756fa8071
Habib Afolabi <habibwritescode@gmail.com>
2021-01-15 07:53:29 +0100

    REname to app.js and add procfile

--- app.js
+++ app.js
@@ -3,44 +3,51 @@ const app = express();
 const port = process.env.PORT || 3000;
 const fetch = require('node-fetch');
 
-function error(status, msg) {
-  var err = new Error(msg);
-  err.status = status;
-  return err;
-}
+// function error(status, msg) {
+//   var err = new Error(msg);
+//   err.status = status;
+//   return err;
+// }
 
 let rates;
 
-app.get('/api/rates', (req, res) => {
-  const baseCurrency = req.query.base;
-  const currencies = req.query.currency.split(',');
-
+app.get('/api/rates/', (req, res, next) => {
+  const {base, currency} = req.query
+  console.log('bc',base, currency)
+  
+  if(!base || !currency){
+    res.status(400).json({
+      error: 'Some required parameters are missing'
+    })
+  }
+  
   async function getApiResults() {
     try {
-      const response = await fetch(`https://api.exchangeratesapi.io/latest?base=${baseCurrency}`)
+      const response = await fetch(`https://api.exchangeratesapi.io/latest?base=${base}`)
       const json = await response.json()
       rates = await json.rates;
-      console.log(rates)
+      // console.log(rates)
     } catch (error) {
       console.error(error)
     }
     return rates;
   }
   getApiResults()
-
+  
   const date = new Date();
   const dd = String(date.getDate()).padStart(2, '0');
   const mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
   const yyyy = date.getFullYear();
-
+  
   const today = `${yyyy}-${mm}-${dd}`;
-
-  console.log('td',today);
-  console.log('0', currencies[0]);
-
+  
+  // console.log('td',today);
+  // console.log('0', currencies[0]);
+  const currencies = currency.split(',');
+  
   res.json({
     "results": {
-      "base": `${baseCurrency}`,
+      "base": `${base}`,
       "date": `${today}`,
       "rates": {
         [currencies[0]]: rates[currencies[0]],
@@ -49,8 +56,7 @@ app.get('/api/rates', (req, res) => {
       }
     }
   })
-  console.log(req.query)
-  console.log(currencies)
+  // console.log(req.query)
 })
 
 app.listen(port, () => {
